c020bd4932dd05504f39d4b4d83bc9d9e5ce6fc9
devEmran <alemrandev@gmail.com>
2025-01-23 02:32:58 +0000

    minor changes

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,15 @@
 <script setup lang="ts">
+import { ref } from 'vue'
+const counter = ref({
+  count: 5
+})
 
+let { count } = counter.value
+count++
 </script>
 
 <template>
   <main>
-    <div>This is home page</div>
+    <div>This is count  {{  counter.count }} {{ count }}</div>
   </main>
 </template>
